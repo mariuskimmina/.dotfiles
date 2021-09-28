a6b087e70ad8e6ebac7adb7932a626ca1855cbd4
m.kimmina <m.kimmina@qgroup.de>
2021-09-28 12:57:21 +0200

    improve lsp config

--- nvim/.config/nvim/lua/remaps.lua
+++ nvim/.config/nvim/lua/remaps.lua
@@ -15,6 +15,9 @@ api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
 api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true, silent = true })
 api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true, silent = true })
 
+-- Nvim tree
 api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Telescope
 api.nvim_set_keymap('n', '<Leader>ff',  [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 api.nvim_set_keymap('n', '<Leader>fg',  [[<Cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
