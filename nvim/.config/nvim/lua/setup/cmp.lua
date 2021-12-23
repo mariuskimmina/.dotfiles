5014ab069da9f66493274c935b0497dff878de62
Mindslave <mindslave.blog@protonmail.com>
2021-12-23 08:23:52 +0100

    Improved Neovim config

--- nvim/.config/nvim/lua/setup/cmp.lua
+++ nvim/.config/nvim/lua/setup/cmp.lua
@@ -9,10 +9,17 @@ if not snip_status_ok then
   return
 end
 
+
+
 local lspkind = require 'lspkind'
 lspkind.init()
 
 require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
 
 local kind_icons = {
   Text = "",
@@ -42,7 +49,6 @@ local kind_icons = {
   TypeParameter = "",
 }
 
-
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -50,11 +56,45 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        ["C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-e>"] = cmp.mapping {
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        },
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
     },
     sources = {
         { name = "luasnip" },
@@ -68,6 +108,7 @@ cmp.setup({
             with_text = true,
             maxwidth = 60,
             menu = {
+                luasnip = "[SNIP]",
                 buffer = "[BUF]",
                 nvim_lsp = "[LSP]",
                 nvim_lua = "[VIM]",
@@ -75,9 +116,16 @@ cmp.setup({
             }
         }
     },
+    confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    documentation = {
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
     experimental = {
         native_menu = false,
-        ghost_text = true,
+        ghost_text = false,
     },
 })
 
