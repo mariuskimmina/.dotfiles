d69413ea92c9a6a2dd96319f8ab79e8caacb4564
Marius Kimmina <mar.kimmina@gmail.com>
2022-05-05 14:52:12 +0200

    improve config

--- nvim/.config/nvim/lua/setup/cmp.lua
+++ nvim/.config/nvim/lua/setup/cmp.lua
@@ -14,39 +14,6 @@ lspkind.init()
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
-local kind_icons = {
-    Text = "",
-    Method = "m",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
-}
-
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -54,12 +21,8 @@ cmp.setup({
         end,
     },
     mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ["<C-e>"] = cmp.mapping {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
@@ -71,7 +34,7 @@ cmp.setup({
         { name = "luasnip" },
         { name = 'nvim_lua' },
         { name = 'path' },
-        { name = 'buffer', keyword_length = 4 },
+        { name = 'buffer', keyword_length = 3 },
     },
     formatting = {
         format = lspkind.cmp_format {
