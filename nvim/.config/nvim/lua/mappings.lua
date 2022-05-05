a3bb8fdea8f1a5b25643448c6736c0102b0ed480
Marius Kimmina <mar.kimmina@gmail.com>
2022-05-05 14:01:05 +0200

    improve config

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -22,29 +22,6 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 -- Get into normal mode comfortably
 keymap("i", "jjk", "<ESC>", opts)
 
-
--- LSP
--- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
--- keymap("n", "gR", "<cmd>lua vim.lsp.buf.references<CR>", opts)
-
--- keymap("n", "gt", "<cmd> Lspsaga hover_doc<CR>", opts)
--- keymap("n", "gh", "<cmd> Lspsaga lsp_finder<CR>", opts)
--- keymap("n", "gs", "<cmd> Lspsaga signature_help<CR>", opts)
--- keymap("n", "gr", "<cmd> Lspsaga rename<CR>", opts)
--- nnoremap gd :lua vim.lsp.buf.definition()<CR>
--- nnoremap gR :lua vim.lsp.buf.references()<CR>
--- nnoremap <silent>gt :Lspsaga hover_doc<CR>
--- nnoremap <silent>gh :Lspsaga lsp_finder<CR>
--- nnoremap <silent>gs :Lspsaga signature_help<CR>
--- nnoremap <silent>gr :Lspsaga rename<CR>
--- nnoremap <silent><leader>ca :Lspsaga code_action<CR>
--- vnoremap <silent><leader>ca :<C-U>Lspsaga range_code_action<CR>
--- nnoremap <silent><leader>cd :Lspsaga show_line_diagnostics<CR>
--- nnoremap <silent>[e :Lspsaga diagnostic_jump_next<CR>
--- nnoremap <silent>]e :Lspsaga diagnostic_jump_prev<CR>
--- nnoremap <silent><A-d> :Lspsaga open_floaterm<CR>
--- tnoremap <silent><A-d> <C-\><C-n>:Lspsaga close_floaterm<CR><
-
 -- Telescope
 keymap("n", "<leader>tf", '<cmd>lua require("telescope.builtin").find_files(require("telescope.themes").get_dropdown({ previewer = false }))<cr>', opts)
 keymap("n", "<leader>tg", '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
@@ -53,10 +30,6 @@ keymap("n", "<leader>tos", '<cmd>lua require("setup.telescope").search_opensourc
 -- neoclip
 -- Having more stuff on my clipboard
 keymap("n", "<leader>p", [[<Cmd>lua require('telescope').extensions.neoclip.plus()<CR>]], opts)
-
--- Barbar
--- keymap("n", "<leader>h", ":BufferPrevious<CR>", opts)
--- keymap("n", "<leader>l", ":BufferNext<CR>", opts)
 
 -- NvimTree
 keymap("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
--- nvim/.config/nvim/lua/setup/alpha.lua
+++ nvim/.config/nvim/lua/setup/alpha.lua
@@ -2,42 +2,42 @@ local dashboard = require"alpha.themes.dashboard"
 math.randomseed(os.time())
 
 local function button(sc, txt, keybind, keybind_opts)
-  local b = dashboard.button(sc, txt, keybind, keybind_opts)
-  b.opts.hl = "Function"
-  b.opts.hl_shortcut = "Type"
-  return b
+    local b = dashboard.button(sc, txt, keybind, keybind_opts)
+    b.opts.hl = "Function"
+    b.opts.hl_shortcut = "Type"
+    return b
 end
 
 local function pick_color()
-  local colors = {"String", "Identifier", "Keyword", "Number"}
-  return colors[math.random(#colors)]
+    local colors = {"String", "Identifier", "Keyword", "Number"}
+    return colors[math.random(#colors)]
 end
 
 local function footer()
-  local total_plugins = #vim.tbl_keys(packer_plugins)
-  local vim_version = vim.version()
-  return total_plugins .. " plugins installed | Neovim Version: " .. vim_version['major'] .. '.' .. vim_version['minor'] .. '.' .. vim_version['patch']
+    local total_plugins = #vim.tbl_keys(packer_plugins)
+    local vim_version = vim.version()
+    return total_plugins .. " plugins installed | Neovim Version: " .. vim_version['major'] .. '.' .. vim_version['minor'] .. '.' .. vim_version['patch']
 end
 
 dashboard.section.header.val = {
-  [[ _______             ____   ____.__         ]],
-  [[ \      \   ____  ___\   \ /   /|__| _____  ]],
-  [[ /   |   \_/ __ \/  _ \   Y   / |  |/     \ ]],
-  [[/    |    \  ___(  <_> )     /  |  |  Y Y  \]],
-  [[\____|__  /\___  >____/ \___/   |__|__|_|  /]],
-  [[        \/     \/                        \/ ]],
+    [[ _______             ____   ____.__         ]],
+    [[ \      \   ____  ___\   \ /   /|__| _____  ]],
+    [[ /   |   \_/ __ \/  _ \   Y   / |  |/     \ ]],
+    [[/    |    \  ___(  <_> )     /  |  |  Y Y  \]],
+    [[\____|__  /\___  >____/ \___/   |__|__|_|  /]],
+    [[        \/     \/                        \/ ]],
 }
 dashboard.section.header.opts.hl = pick_color()
 
 dashboard.section.buttons.val = {
-  button("SPC t o s", "  open-source projects"),
-  button("SPC f f", "  personal projects"),
-  button("SPC t f", "  find file"),
-  button("SPC f a", "  open folder"),
-  button("SPC s l", "  Open session"),
-  button("SPC c n", "  New file"),
-  button("SPC c c", "  Update plugins"),
-  button("q", "  Quit", "<Cmd>qa<CR>")
+    button("SPC t o s", "  open-source projects"),
+    button("SPC f f", "  personal projects"),
+    button("SPC t f", "  find file"),
+    button("SPC f a", "  open folder"),
+    button("SPC s l", "  Open session"),
+    button("SPC c n", "  New file"),
+    button("SPC c c", "  Update plugins"),
+    button("q", "  Quit", "<Cmd>qa<CR>")
 }
 
 dashboard.section.footer.val = footer()
@@ -47,8 +47,8 @@ require"alpha".setup(dashboard.opts)
 
 -- hide tabline on startup screen
 vim.cmd [[
-augroup alpha_tabline
-  au!
-  au FileType alpha set showtabline=0 | au BufUnload <buffer> set showtabline=2
-augroup END
+    augroup alpha_tabline
+        au!
+        au FileType alpha set showtabline=0 | au BufUnload <buffer> set showtabline=2
+    augroup END
 ]]
