1279a8f01267fae9e585c4adec4b85fb28d42bcb
Marius Kimmina <mar.kimmina@gmail.com>
2022-05-15 12:48:21 +0200

    trying to implement some kind of autosave

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -12,3 +12,9 @@ vim.api.nvim_exec([[
     au FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
   augroup END
   ]], false)
+
+
+-- better autosaving
+vim.api.nvim_create_autocmd("TextChanged", { command = "update" })
+vim.api.nvim_create_autocmd("FocusLost", { command = "update" })
+vim.api.nvim_create_autocmd("BufEnter", { command = "update" })
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -28,6 +28,8 @@ keymap("n", "<leader>tg", '<cmd>lua require("telescope.builtin").live_grep()<cr>
 keymap("n", "<leader>tos", '<cmd>lua require("setup.telescope").search_opensource()<CR>',opts)
 vim.keymap.set("n", "<leader>gf", "<cmd>Telescope diagnostics<cr>", {buffer=0})
 
+vim.keymap.set("i", "<esc>", "<esc>:update<cr>", { silent = true })
+
 -- neoclip
 -- Having more stuff on my clipboard
 keymap("n", "<leader>p", [[<Cmd>lua require('telescope').extensions.neoclip.plus()<CR>]], opts)
