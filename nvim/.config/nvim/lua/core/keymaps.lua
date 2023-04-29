97e973d03a51a6aa6de30f1bfb6526cc7ce65e10
Marius Kimmina <mar.kimmina@gmail.com>
2023-04-29 09:17:30 +0200

    add wezterm config

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 
 local opts2 = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
+-- local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
---
+++ term/.config/wezterm/wezterm.lua
@@ -0,0 +1,4 @@
+return {
+    color_scheme = "Catppuccin Mocha",
+    hide_tab_bar_if_only_one_tab = true
+}
