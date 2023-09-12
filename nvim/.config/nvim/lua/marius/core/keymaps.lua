3e9e59208bddd3fd6e8d2893908a34f3676a341d
Marius Kimmina <mar.kimmina@gmail.com>
2023-09-12 11:43:03 +0200

    improve

--- nvim/.config/nvim/lua/marius/core/keymaps.lua
+++ nvim/.config/nvim/lua/marius/core/keymaps.lua
@@ -1,7 +1,13 @@
+local Util = require("core.util")
+
 -- set leader key to space
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+
+local opts2 = { noremap = true, silent = true }
+
+-- local term_opts = { silent = true }
 
 ---------------------
 -- General Keymaps -------------------
@@ -11,3 +17,13 @@ local keymap = vim.keymap -- for conciseness
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- Resize with arrows
+keymap.set("n", "<C-Up>", ":resize -2<CR>", opts2)
+keymap.set("n", "<C-Down>", ":resize +2<CR>", opts2)
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts2)
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts2)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts2)
+keymap("v", ">", ">gv", opts2)
