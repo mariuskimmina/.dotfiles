c691844955a07e825716c8fa11dce87678b28bbb
Marius Kimmina <mar.kimmina@gmail.com>
2023-10-11 07:56:18 +0200

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 return {
 	color_scheme = "Catppuccin Macchiato",
+	font = require("wezterm").font("MesloLGS NF"),
 	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
+	-- window_decorations = "RESIZE",
 	window_padding = {
 		left = 0,
 		right = 0,
