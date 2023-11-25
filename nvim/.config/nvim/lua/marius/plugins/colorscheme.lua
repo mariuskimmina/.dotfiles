bee93e5031c8bc6458aaf0ad5a64a7d377a86887
Marius Kimmina <mar.kimmina@gmail.com>
2023-11-25 12:06:03 +0100

    macchiato

--- nvim/.config/nvim/lua/marius/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/marius/plugins/colorscheme.lua
@@ -4,6 +4,11 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      local catppuccin = require("catppuccin")
+
+      catppuccin.setup({
+        flavour = "macchiato",
+      })
       -- load the colorscheme here
       vim.cmd([[colorscheme catppuccin]])
     end,
