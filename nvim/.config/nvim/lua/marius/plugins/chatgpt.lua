666143b5714a68e7c1953d99a726cfedc84e2038
Marius Kimmina <mar.kimmina@gmail.com>
2023-12-15 10:06:32 +0100

    new plugin

---
+++ nvim/.config/nvim/lua/marius/plugins/chatgpt.lua
@@ -0,0 +1,14 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "bw get password ChatGPT API key OpenAI Neovim",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
