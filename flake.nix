9dfda3d27516d3ca3b2d7c060c6c46aab24f0493
MariusKimmina <mar.kimmina@gmail.com>
2024-04-06 10:53:06 +0200

    mores stuff

--- flake.nix
+++ flake.nix
@@ -10,13 +10,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
-        inputs.home-manager.nixosModules.default
       ];
+    };
+    homeConfigurations = {
+      marius = home-manager.lib.homeManagerConfiguration {
+      modules = [
+        ./home.nix
+      ];
+      };
     };
   };
 }
