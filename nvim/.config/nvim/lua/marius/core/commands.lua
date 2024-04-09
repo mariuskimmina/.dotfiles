9b2d6f925857c000c532a9830c95fdfe243f3bbc
Marius Kimmina <marius@adjoe.io>
2024-04-09 12:18:47 +0200

    fix

--- nvim/.config/nvim/lua/marius/core/commands.lua
+++ nvim/.config/nvim/lua/marius/core/commands.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+local function get_git_root()
+  local dot_git_path = vim.fn.finddir(".git", ".;")
+  return vim.fn.fnamemodify(dot_git_path, ":h")
+end
+
+vim.api.nvim_create_user_command("CdGitRoot", function()
+  vim.api.nvim_set_current_dir(get_git_root())
+end, {})
