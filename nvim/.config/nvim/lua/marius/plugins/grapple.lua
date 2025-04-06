99abda323931e9816dfb11ae31264569603a6977
Marius Kimmina <mar.kimmina@gmail.com>
2025-04-06 16:08:47 +0200

    replace harpoon with grapple

---
+++ nvim/.config/nvim/lua/marius/plugins/grapple.lua
@@ -0,0 +1,19 @@
+return {
+  "cbochs/grapple.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  },
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>m", require("grapple").toggle)
+    keymap.set("n", "ms", require("grapple").toggle_tags)
+    keymap.set("n", "mn", "<cmd>Grapple cycle_tags next<cr>")
+    keymap.set("n", "mp", "<cmd>Grapple cycle_tags prev<cr>")
+    vim.keymap.set("n", "m1", "<cmd>Grapple select index=1<cr>")
+    vim.keymap.set("n", "m2", "<cmd>Grapple select index=2<cr>")
+    vim.keymap.set("n", "m3", "<cmd>Grapple select index=3<cr>")
+    vim.keymap.set("n", "m4", "<cmd>Grapple select index=4<cr>")
+  end,
+}
--- nvim/.config/nvim/lua/marius/plugins/lualine.lua
+++ nvim/.config/nvim/lua/marius/plugins/lualine.lua
@@ -1,29 +1,55 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local clients_lsp = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      ---@diagnostic disable-next-line: deprecated
+      local clients = vim.lsp.buf_get_clients(bufnr)
+      if next(clients) == nil then
+        return ""
+      end
 
+      local c = {}
+      for _, client in pairs(clients) do
+        table.insert(c, client.name)
+      end
+      return "\u{f085} " .. table.concat(c, "|")
+    end
+
+    local branch = { "branch", icon = "" }
+    local mode = { "mode", icon = "󰡛" }
+    local diagnostics = { "diagnostics", symbols = { error = " ", warn = " ", info = " " }, colored = false }
     lualine.setup({
       options = {
         theme = "catppuccin",
+        icons_enabled = true,
+        disabled_filetypes = {},
+        section_separators = {},
+        component_separators = {},
+        always_divide_middle = true,
+        globalstatus = false,
       },
       sections = {
-        lualine_x = {
+        lualine_a = { mode },
+        lualine_b = { branch, "filename" },
+        lualine_c = {
           {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
+            function()
+              return require("grapple").name_or_index()
+            end,
+            cond = function()
+              return package.loaded["grapple"] and require("grapple").exists()
+            end,
           },
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
         },
+        lualine_x = { diagnostics, "diff", "filetype" },
+        lualine_y = { "progress", "location" },
+        lualine_z = { clients_lsp },
       },
     })
   end,
